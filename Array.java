bcac286db6efe5256cbe3033dd9ca9c739c4d19d
ganeshdhere90 <142479026+ganeshdhere90@users.noreply.github.com>
2023-08-19 12:29:38 +0530

    array all shoeing program

--- Array.java
+++ Array.java
@@ -1,8 +1,11 @@
 public class Array{
 public static void main(String[] arg){
 
-  String name[]={"sham","ram","shital","gita"};
-System.out.println(name[1]);
+  int number[]={1,2,3,4,5,6,7,8,9,24};
+System.out.println(number[7]);
+for(int i=0;i<=number.length;i++){
+   System.out.println(number[i]);
+}
 }
 
 }
